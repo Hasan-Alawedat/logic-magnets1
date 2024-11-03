e3249b0d6e52fb7b4ea480cbf8ec8a866f37d7a2
Hasan-Alawedat <153757426+Hasan-Alawedat@users.noreply.github.com>
2024-11-03 21:44:38 +0300

    Add files via upload

---
+++ logic/phases.py
@@ -0,0 +1,75 @@
+levels = [
+    {
+        "targets": [(1, 3), (1, 5)],
+        "pieces": [
+            {"type": "purple", "position": (2, 2)},
+            {"type": "gray", "position": (1, 4)},
+        ],
+        "move": 2  
+    },
+    {
+        "targets": [(0, 2), (2, 0), (2, 2), (2, 4), (4, 2)],
+        "pieces": [
+            {"type": "gray", "position": (1, 2)},
+            {"type": "gray", "position": (2, 1)},
+            {"type": "gray", "position": (2, 3)},
+            {"type": "gray", "position": (3, 2)},
+            {"type": "purple", "position": (4, 0)}
+        ],
+        "move": 3
+    },
+     
+     {
+        "targets": [(0, 5), (2, 5)],
+        "pieces": [
+            {"type": "purple", "position": (2, 0)},
+            {"type": "gray", "position": (1, 4)},
+        ],
+        "move": 2 
+    },
+
+    {
+        "targets": [(2, 0), (2, 1), (2, 2), (2, 3), (2, 5)],
+        "pieces": [
+            {"type": "purple", "position": (4, 2)},
+            {"type": "red", "position": (4, 3)},
+            {"type": "gray", "position": (0, 3)},
+            {"type": "gray", "position": (2, 0)},
+            {"type": "gray", "position": (2, 2)},
+
+        ],
+        "move": 5 
+    },
+     {
+        "targets": [(0, 0), (0, 2), (4, 1)],
+        "pieces": [
+            {"type": "purple", "position": (2, 0)},
+            {"type": "gray", "position": (1, 1)},
+            {"type": "gray", "position": (3, 1)},
+        ],
+        "move": 2  
+    },
+     {
+        "targets": [(0, 0), (1, 0), (0, 2), (1, 2), (3, 0)],
+        "pieces": [
+            {"type": "purple", "position": (3, 1)},
+            {"type": "gray", "position": (1, 0)},
+            {"type": "gray", "position": (1, 2)},
+            {"type": "gray", "position": (2, 0)},
+            {"type": "gray", "position": (2, 2)},
+
+
+        ],
+        "move": 2  
+    },
+     {
+        "targets": [(0, 3), (1, 2), (2, 3)],
+        "pieces": [
+            {"type": "purple", "position": (2, 0)},
+            {"type": "gray", "position": (1, 1)},
+            {"type": "gray", "position": (1, 3)},
+        ],
+        "move": 2  
+    },
+    
+]
